1ea85453c6140a3cea23f7dff70807cf72c861d7
Hieu Mai <hieumt2@fsoft.com.vn>
2019-02-15 11:42:29 +0700

    add unit test

--- tests/test_request_handler.py
+++ tests/test_request_handler.py
@@ -9,48 +9,48 @@ class TestRequestHandlerParams(TestCase):
     def test_url_is_not_valid(self):
         """Check base_url valid"""
         with assert_raises(Exception) as cm:
-            RequestHandler('google', '1', '2')
-            assert_true('The base_url provided is not valid.' in str(cm.exception))
+            RequestHandler("google", "1", "2")
+            assert_true("The base_url provided is not valid." in str(cm.exception))
 
     def test_app_id_is_not_valid(self):
         """Check app_id valid"""
         with assert_raises(Exception) as cm:
-            RequestHandler('http://google.com', 1, '2')
-            assert_true('The app_id provided is not valid.' in str(cm.exception))
+            RequestHandler("http://google.com", 1, "2")
+            assert_true("The app_id provided is not valid." in str(cm.exception))
 
     def test_app_key_is_not_valid(self):
         """Check app_key valid"""
         with assert_raises(Exception) as cm:
-            RequestHandler('http://google.com', '1', 2)
-            assert_true('The app_key provided is not valid.' in str(cm.exception))
+            RequestHandler("http://google.com", "1", 2)
+            assert_true("The app_key provided is not valid." in str(cm.exception))
 
     def test_initial_success(self):
         """Check initial"""
-        request_handler = RequestHandler('http://google.com', '1', '2')
-        assert_equal(request_handler.base_url, 'http://google.com')
-        assert_equal(request_handler.app_id, '1')
-        assert_equal(request_handler.app_key, '2')
+        request_handler = RequestHandler("http://google.com", "1", "2")
+        assert_equal(request_handler.base_url, "http://google.com")
+        assert_equal(request_handler.app_id, "1")
+        assert_equal(request_handler.app_key, "2")
 
 
 class TestRequestHandler(TestCase):
     """Test Request Handler"""
     def setUp(self):
-        self.request_handler = RequestHandler('http://google.com', '1', '2')
+        self.request_handler = RequestHandler("http://google.com", "1", "2")
 
     def tearDown(self):
         self.request_handler = None
 
     def test_send_get_request(self):
         """Check send get request successfully"""
-        with patch('factom_sdk.request_handler.request_handler.requests.get') as mock_get:
+        with patch("factom_sdk.request_handler.request_handler.requests.get") as mock_get:
             mock_get.return_value.ok = True
-            response = self.request_handler.get('')
+            response = self.request_handler.get("")
             assert_is_not_none(response)
 
     def test_send_post_request(self):
         """Check Send post request successfully"""
-        with patch('factom_sdk.request_handler.request_handler.requests.post') as mock_post:
+        with patch("factom_sdk.request_handler.request_handler.requests.post") as mock_post:
             mock_post.return_value.ok = True
-            response = self.request_handler.post('')
+            response = self.request_handler.post("")
             assert_is_not_none(response)
 
