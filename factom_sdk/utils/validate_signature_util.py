2429b1f2b50cfe53d1d1ca75c574c676b1eb29e7
Hieu Mai <hieumt2@gmail.com>
2019-02-26 16:30:37 +0700

    set-2

--- factom_sdk/utils/validate_signature_util.py
+++ factom_sdk/utils/validate_signature_util.py
@@ -15,22 +15,23 @@ class ValidateSignatureUtil:
 
         if len(external_ids) < 6 or external_ids[0] != type_name or external_ids[1] != "0x01":
             return invalid_format
-        else:
-            signer_chain_id = external_ids[2]
-            signer_public_key = external_ids[3]
-            signature = codecs.encode(codecs.decode(external_ids[4], "hex"), "base64").decode()
-            time_stamp = external_ids[5]
-            data = {}
-            if "dblock" in obj["data"] \
-                    and obj["data"]["dblock"] is not None \
-                    and "height" in obj["data"]["dblock"]:
-                data["active_at_height"] = obj["data"]["dblock"]["height"]
-            key_response = request_handler.get("/".join([factom_sdk.utils.consts.IDENTITIES_URL, signer_chain_id,
-                                                         factom_sdk.utils.consts.KEYS_STRING]), data)
-            if len([item for item in key_response["data"] if item["key"] == signer_public_key]) == 0:
-                return "inactive_key"
-            else:
-                message = signer_chain_id + obj["data"]["content"] + time_stamp
-                if not KeyUtil.validate_signature(signer_public_key, signature, message):
-                    return "invalid_signature"
+
+        signer_chain_id = external_ids[2]
+        signer_public_key = external_ids[3]
+        signature = codecs.encode(codecs.decode(external_ids[4], "hex"), "base64").decode()
+        time_stamp = external_ids[5]
+        data = {}
+        if "dblock" in obj["data"] \
+                and obj["data"]["dblock"] is not None \
+                and "height" in obj["data"]["dblock"]:
+            data["active_at_height"] = obj["data"]["dblock"]["height"]
+        key_response = request_handler.get("/".join([factom_sdk.utils.consts.IDENTITIES_URL, signer_chain_id,
+                                                     factom_sdk.utils.consts.KEYS_STRING]), data)
+        if len([item for item in key_response["data"] if item["key"] == signer_public_key]) == 0:
+            return "inactive_key"
+
+        message = signer_chain_id + obj["data"]["content"] + time_stamp
+        if not KeyUtil.validate_signature(signer_public_key, signature, message):
+            return "invalid_signature"
+
         return "valid_signature"
