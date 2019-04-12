8cb4c662d29d76563f9ff3c280ba0c754ac344bc
Kirit Thadaka <kirit.thadaka@zero.ai>
2019-04-12 01:36:58 -0700

    Changed conf

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'pyharmony'
+project = 'factom-harmony-connect-python-sdk'
 copyright = '2019, Factom Inc.'
 author = 'Factom Inc.'
 
