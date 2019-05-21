4d76b6ee4488dc6015e6ca61e706bc40d0c9f0bb
Hieu Mai <lionvnam@gmail.com>
2019-05-21 09:47:11 +0700

    Apply kwargs for achors and receipts client

--- factom_sdk/client/anchors_client.py
+++ factom_sdk/client/anchors_client.py
@@ -6,7 +6,7 @@ class AnchorsClient:
     def __init__(self, base_url: str, app_id: str, app_key: str):
         self.request_handler = RequestHandler(base_url, app_id, app_key)
 
-    def get(self, object_hash: str = None, height: int = None):
+    def get(self, **kwargs):
         """Gets the anchors for a specific entry or directory block from Connect.
 
         Args:
@@ -16,11 +16,18 @@ class AnchorsClient:
         Returns:
             Anchors object.
         """
+        base_url = kwargs.get("base_url")
+        app_id = kwargs.get("app_id")
+        app_key = kwargs.get("app_key")
+        object_hash = kwargs.get("object_hash")
+        height = kwargs.get("height")
         if object_hash is None:
             assert height is not None, "either object_hash or height must not be None"
             assert isinstance(height, int) and height > 0, "height must be a positive integer"
-            return self.request_handler.get("/".join([factom_sdk.utils.consts.ANCHORS_URL, str(height)]))
+            return self.request_handler.get("/".join([factom_sdk.utils.consts.ANCHORS_URL, str(height)]),
+                                            base_url=base_url, app_id=app_id, app_key=app_key)
         else:
             assert isinstance(object_hash, str) and len(object_hash) == 64, "object_hash must be a string of length 64"
             assert height is None, "object_hash provided, height must be None"
-            return self.request_handler.get("/".join([factom_sdk.utils.consts.ANCHORS_URL, object_hash]))
+            return self.request_handler.get("/".join([factom_sdk.utils.consts.ANCHORS_URL, object_hash]),
+                                            base_url=base_url, app_id=app_id, app_key=app_key)
