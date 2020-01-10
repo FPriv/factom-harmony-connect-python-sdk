1bedff2624385b78ad2eac3ef566ff6882483152
Alberto Yanes <albertoyanesjr3@hotmail.com>
2020-01-10 10:35:41 -0300

    Fixed comments

--- factom_sdk/utils/utils.py
+++ factom_sdk/utils/utils.py
@@ -14,15 +14,15 @@ class Utils:
             A key pair object with Public and Private keys.
         """
         return KeyCommon.create_key_pair()
-
+raw_private_key: str = None
     @staticmethod
-    def convert_raw_to_key_pair():
+    def convert_raw_to_key_pair(raw_private_key):
         """Convert standard ed25519 keys into idpub/idsec formatted keys.
 
         Returns:
             A key bytes array
         """
-        return KeyCommon.create_key_pair()
+        return KeyCommon.create_key_pair(raw_private_key)
 
     @staticmethod
     def convert_to_raw(signer_key: str):
